83f3f6c79bb4d655df92a5b8ae49baf780be89fa
¨nikotov¨ <nicolastovar18@hotmail.com>
2026-04-16 10:09:29 -0500

    now istg

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,11 +1,11 @@
-from app.calculator import sum, resta, multiply
+from app.calculator import suma, resta, multiplicacion
 
 def test_sum() -> None:
-    assert sum(2, 3) == 5
+    assert suma(2, 3) == 5
 
 # Introducir un fallo premeditado!
 def test_resta() -> None:
     assert resta(5, 3) == 2
 
 def test_multiply() -> None:
-    assert multiply(2, 3) == 6
+    assert multiplicacion(2, 3) == 6
